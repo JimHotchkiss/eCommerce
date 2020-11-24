d480f784a67e1304a340a8f60942c3997dc8e182
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-11-24 10:18:34 -0800

    Adding Footer, Header and styling.

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Navbar, Nav, Container } from "react-bootstrap"
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>OnABender</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>
+                <i class='fas fa-shopping-cart px-1'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i class='fas fa-user px-1'></i> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header

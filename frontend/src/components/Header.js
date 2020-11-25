539da3ceab6a8020928f34821f903abba32b3ec8
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-11-24 17:18:51 -0800

    Couple small changes.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,9 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>OnABender</Navbar.Brand>
+          <Navbar.Brand href='/'>
+            OnA<span className='text-primary'>Bender</span>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>

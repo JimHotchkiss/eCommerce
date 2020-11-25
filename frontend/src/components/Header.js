67815757be913faa4b5ec7afec1542cb3b1f4de0
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-11-25 09:36:07 -0800

    Added react-route-domn and react-router-bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,32 @@
 import React from "react"
 import { Navbar, Nav, Container } from "react-bootstrap"
+import { LinkContainer } from "react-router-bootstrap"
+
+// LinkContainer is from react-router-bootstrap and works very much the same was as Link from react-router-dom
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            OnA<span className='text-primary'>Bender</span>
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              OnA<span className='text-primary'>Bender</span>
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart px-1'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user px-1'></i> Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart px-1'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='./login'>
+                <Nav.Link>
+                  <i className='fas fa-user px-1'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,21 +1,24 @@
 import React from "react"
 import { Card } from "react-bootstrap"
 import Rating from "./Rating"
+import { Link } from "react-router-dom"
+
+// Use Link to replace anchor tags - instead of href use 'to'
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       {/* {console.log(product._id)} */}
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         {/* {console.log(product.image)} */}
         <Card.Img src={product.image} variant='top' />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='div'>
           <Rating
             value={product.rating}
